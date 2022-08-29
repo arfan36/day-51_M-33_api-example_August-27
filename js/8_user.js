4c31a9a9061504ba063d82751d73029de1e69060
arfan36 <arfanur36@gmail.com>
2022-08-29 17:51:46 +0600

    summary

--- js/8_user.js
+++ js/8_user.js
@@ -21,3 +21,9 @@ const loadUserAsync = async () => {
 const displayUser = user => {
     console.log(user);
 };
+
+
+const p = [1, 2, 3];
+const q = p.map(n => Math.pow(n, 3));
+console.log(q);
+// const q=p.( n=> Math.pow(n, 3));
